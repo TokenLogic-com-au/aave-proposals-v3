89c5088e60da5814be800121ad820ba45c0aeb69
Juan Ignacio Ubeira <juani@balancer.finance>
2026-05-26 14:45:31 -0300

    Fix main monad script.

--- src/20260518_Multi_GhoMonadActivation/AaveV3Monad_GhoMonadActivation_20260518.sol
+++ src/20260518_Multi_GhoMonadActivation/AaveV3Monad_GhoMonadActivation_20260518.sol
@@ -5,13 +5,13 @@ import {AaveV3GHOLaunch} from '../helpers/gho-launch/AaveV3GHOLaunch.sol';
 import {GhoCCIPChains} from '../helpers/gho-launch/constants/GhoCCIPChains.sol';
 
 /**
- * @title Gho X-Layer Launch
+ * @title Gho Monad Launch
  * @author @TokenLogic
  * - Snapshot: TODO
  * - Discussion: TODO
  */
-contract AaveV3XLayer_GhoMonadActivation_20260518 is AaveV3GHOLaunch {
-  constructor() AaveV3GHOLaunch(GhoCCIPChains.XLAYER()) {}
+contract AaveV3Monad_GhoMonadActivation_20260518 is AaveV3GHOLaunch {
+  constructor() AaveV3GHOLaunch(GhoCCIPChains.MONAD()) {}
   function _setupGhoAaveSteward() internal override {
     // Do not setup Aave Core Steward, will be set up with Gho launch on Aave.
   }
