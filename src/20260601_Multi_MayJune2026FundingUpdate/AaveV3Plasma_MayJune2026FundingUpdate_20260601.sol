6c0f833f73e1f08d696cf00215bf34b5c7ee5304
Juan Ignacio Ubeira <juani@tokenlogic.xyz>
2026-06-02 12:22:29 -0300

    Nit: plasmascan.

--- src/20260601_Multi_MayJune2026FundingUpdate/AaveV3Plasma_MayJune2026FundingUpdate_20260601.sol
+++ src/20260601_Multi_MayJune2026FundingUpdate/AaveV3Plasma_MayJune2026FundingUpdate_20260601.sol
@@ -12,7 +12,7 @@ import {IProposalGenericExecutor} from 'aave-helpers/src/interfaces/IProposalGen
  * - Discussion: https://governance.aave.com/t/direct-to-aip-may-june-2026-funding-update/25000
  */
 contract AaveV3Plasma_MayJune2026FundingUpdate_20260601 is IProposalGenericExecutor {
-  // https://etherscan.io/address/0x22740deBa78d5a0c24C58C740e3715ec29de1bFa
+  // https://plasmascan.to/address/0x22740deBa78d5a0c24C58C740e3715ec29de1bFa
   address public constant AFC = 0x22740deBa78d5a0c24C58C740e3715ec29de1bFa;
   uint256 public constant AFC_A_USDT0_ALLOWANCE = 3_000_000e6; // 3M aUSDT, 6 decimals
 
