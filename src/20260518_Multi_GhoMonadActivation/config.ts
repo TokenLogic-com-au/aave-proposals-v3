912ad2d05abb12ca11cb7f9aff61fccd549cbce5
Fermin 'Piscu' Carranza <fcarr004@fiu.edu>
2026-06-22 13:26:40 -0400

    feat: Monad GHO CCIP Lane Activation (#1129)
    * Groundwork: X-Layer CCIP Lanes (#1015).
    
    * Refactor:
    - Proposal: GhoXLayerActivation_20260203 --> GhoMonadActivation_20260518
    - XLayer --> Monad
    - Add XLayer<>Monad remote lanes / remote lanes test
    - Constants: add Monad chain
    
    * Address comments: array size, date.
    
    * Fix main monad script.
    
    * Add LINK token address for monad.
    
    * feat: add block numbers and addresses
    
    * feat: update tests for 1.6 support
    
    * feat: add support for ccip 2.0
    
    * chore: simplify the interface
    
    * feat: update aave-helpers
    
    * feat: update test blocks
    
    * feat: update md
    
    * feat: accept ownership
    
    * feat: merge main
    
    * feat: update md file
    
    * feat: re run all tests
    
    * chore: update default
    
    ---------
    
    Co-authored-by: Juan Ignacio Ubeira <juani@balancer.finance>
    Co-authored-by: Juan Ignacio Ubeira <juani@tokenlogic.xyz>

---
+++ src/20260518_Multi_GhoMonadActivation/config.ts
@@ -0,0 +1,37 @@
+import {ConfigFile} from '../../generator/types';
+export const config: ConfigFile = {
+  rootOptions: {
+    pools: [
+      'AaveV3Ethereum',
+      'AaveV3Avalanche',
+      'AaveV3Arbitrum',
+      'AaveV3Base',
+      'AaveV3Gnosis',
+      'AaveV3Plasma',
+      'AaveV3Mantle',
+      'AaveV3InkWhitelabel',
+      'AaveV3XLayer',
+      'AaveV3Monad',
+    ],
+    title: 'Gho Monad Activation',
+    shortName: 'GhoMonadActivation',
+    date: '20260518',
+    author: '@TokenLogic',
+    discussion: 'https://governance.aave.com/t/arfc-deploy-aave-protocol-on-monad/24943',
+    snapshot:
+      'https://snapshot.box/#/s:aavedao.eth/proposal/0x24f105bd023c476a9b85fa87ff795bfeec769fa799ce6ada8e2724c9738049f6',
+    votingNetwork: 'AVALANCHE',
+  },
+  poolOptions: {
+    AaveV3Ethereum: {configs: {OTHERS: {}}, cache: {blockNumber: 25343705}},
+    AaveV3Avalanche: {configs: {OTHERS: {}}, cache: {blockNumber: 88288750}},
+    AaveV3Arbitrum: {configs: {OTHERS: {}}, cache: {blockNumber: 474743240}},
+    AaveV3Base: {configs: {OTHERS: {}}, cache: {blockNumber: 47493780}},
+    AaveV3Gnosis: {configs: {OTHERS: {}}, cache: {blockNumber: 46781984}},
+    AaveV3Plasma: {configs: {OTHERS: {}}, cache: {blockNumber: 24837240}},
+    AaveV3Mantle: {configs: {OTHERS: {}}, cache: {blockNumber: 96884178}},
+    AaveV3InkWhitelabel: {configs: {OTHERS: {}}, cache: {blockNumber: 48278619}},
+    AaveV3XLayer: {configs: {OTHERS: {}}, cache: {blockNumber: 63129679}},
+    AaveV3Monad: {configs: {OTHERS: {}}, cache: {blockNumber: 82605370}},
+  },
+};
